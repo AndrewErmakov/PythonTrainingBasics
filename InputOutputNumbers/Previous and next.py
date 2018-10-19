ad4455ee0163bbc2c935143bb49fd547e1eeea35
AndrewErmakov <andrew.67@list.ru>
2018-10-19 13:31:55 +0500

    Update Previous and next.py

--- InputOutputNumbers/Previous and next.py
+++ InputOutputNumbers/Previous and next.py
@@ -1,3 +1,4 @@
 number = int(input())
-print("The next number for the number "+str(number)+' '+'is '+str(number+1))
-print('The previous number for the number '+str(number)+' '+'is '+str(number-1))
+print('The next number for the number ' + str(number) + ' ' + 'is ' + str(number+1))
+print('The previous number for the number ' + str(number) + ' ' + 'is ' + str(number-1))
+
