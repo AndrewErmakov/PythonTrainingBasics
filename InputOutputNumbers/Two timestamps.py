7d7c9d739016675ffb70cbf9aa5ee6115642e5b7
AndrewErmakov <andrew.67@list.ru>
2018-10-19 13:37:34 +0500

    Update Two timestamps.py

--- InputOutputNumbers/Two timestamps.py
+++ InputOutputNumbers/Two timestamps.py
@@ -5,3 +5,4 @@ number_of_hours2 = int(input())
 number_of_minutes2 = int(input())
 number_of_seconds2 = int(input())
 print(3600 * (number_of_hours2 - number_of_hours1) + 60 * (number_of_minutes2 - number_of_minutes1) + (number_of_seconds2 - number_of_seconds1))
+
