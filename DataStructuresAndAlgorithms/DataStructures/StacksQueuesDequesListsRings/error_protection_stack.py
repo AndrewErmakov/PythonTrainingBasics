b11c32e2308081438ae4517fa49d692fb6c5677d
Andrew Ermakov <andrew.67@list.ru>
2020-01-16 18:00:07 +0500

    Solving tasks on the topic stacks

---
+++ DataStructuresAndAlgorithms/DataStructures/StacksQueuesDequesListsRings/error_protection_stack.py
@@ -0,0 +1,53 @@
+class SimpleStack:
+    def __init__(self):
+        self.list_numbers = []
+
+    def push(self, number):
+        self.list_numbers.append(number)
+        return 'ok'
+
+    def pop(self):
+        if self.size() == 0:
+            return 'error'
+        else:
+            return self.list_numbers.pop()
+
+    def back(self):
+        if self.size() == 0:
+            return 'error'
+        else:
+            return self.list_numbers[-1]
+
+    def size(self):
+        return len(self.list_numbers)
+
+    def clear(self):
+        self.list_numbers = []
+        return 'ok'
+
+
+if __name__ == '__main__':
+    command = input().split()
+    simple_stack = SimpleStack()
+
+    while 'exit' not in command:
+        if command[0] == 'push':
+            print(simple_stack.push(int(command[1])))
+
+        elif command[0] == 'size':
+            print(simple_stack.size())
+
+        elif command[0] == 'pop':
+            print(simple_stack.pop())
+
+        elif command[0] == 'back':
+            print(simple_stack.back())
+
+        elif command[0] == 'clear':
+            print(simple_stack.clear())
+
+        command = input().split()
+
+    print('bye')
+
+
