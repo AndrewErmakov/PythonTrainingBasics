b1502db75294a9caa8951da9e22a9a93a45d4eb2
Andrew Ermakov <andrew.67@list.ru>
2020-02-09 01:04:26 +0500

    Update simple_deque.py

--- DataStructuresAndAlgorithms/DataStructures/StacksQueuesDequesListsRings/simple_deque.py
+++ DataStructuresAndAlgorithms/DataStructures/StacksQueuesDequesListsRings/simple_deque.py
@@ -4,27 +4,31 @@
 
 class SimpleDeque:
     def __init__(self):
-        self.deque = [None] * 300
-        self.start = self.end = 150
+        self.deque = [None] * 3000
+        self.start = self.end = 1500
 
     def push_back(self, number):
         self.deque[self.end] = number
         self.end += 1
+        self.check()
         return 'ok'
 
     def push_front(self, number):
         self.start -= 1
         self.deque[self.start] = number
+        self.check()
         return 'ok'
 
     def pop_front(self):
         element = self.deque[self.start]
         self.start += 1
+        self.check()
         return element
 
     def pop_back(self):
         element = self.deque[self.end - 1]
         self.end -= 1
+        self.check()
         return element
 
     def front(self):
@@ -34,11 +38,26 @@ class SimpleDeque:
         return self.deque[self.end - 1]
 
     def size_deque(self):
-        return self.end - self.start
+        if self.end < self.start:
+            return len(self.deque) - self.start + self.end
+        else:
+            return self.end - self.start
 
     def clear(self):
-        self.start = self.end = 150
+        self.start = self.end = 1500
         return 'ok'
+
+    def check(self):
+        if self.start < 0:
+            self.start += len(self.deque)
+        if self.end < 0:
+            self.end += len(self.deque)
+
+        if self.start >= len(self.deque):
+            self.start -= len(self.deque)
+
+        if self.end >= len(self.deque):
+            self.end -= len(self.deque)
 
 
 if __name__ == '__main__':
