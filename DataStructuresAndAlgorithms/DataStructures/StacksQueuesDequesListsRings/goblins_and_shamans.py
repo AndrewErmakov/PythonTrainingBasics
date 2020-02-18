aabbac24e3412d1686342467879d318360e5c672
Andrew Ermakov <andrew.67@list.ru>
2020-02-18 22:56:45 +0500

    Solving a task using a module collections

--- DataStructuresAndAlgorithms/DataStructures/StacksQueuesDequesListsRings/goblins_and_shamans.py
+++ DataStructuresAndAlgorithms/DataStructures/StacksQueuesDequesListsRings/goblins_and_shamans.py
@@ -1,78 +1,62 @@
 # Задача №112984. Гоблины и шаманы
 # https://informatics.mccme.ru/mod/statements/view3.php?id=206&chapterid=112984#1
 
-
-from math import ceil
-
-SIZE_QUEUE = 200000
+from collections import deque
 
 
-class UnlimitedQueue:
+class GoblinsDeque:
     def __init__(self):
-        self.list_numbers = [None] * SIZE_QUEUE
-        self.start = self.finish = 0
-        self.max_size_queue = SIZE_QUEUE
+        self.left_part_deque = deque()
+        self.right_part_deque = deque()
         self.cured_numbers_goblins = []
 
-    def push(self, number):
-        if self.check_stack_overflow():
-            self.list_numbers += [None] * self.max_size_queue
-            self.max_size_queue *= 2
+    def equilibrium_condition(self):
+        return len(self.left_part_deque) == len(self.right_part_deque)
 
-        self.list_numbers[self.finish] = number
-        self.finish += 1
-        return 'ok'
-
-    def pop(self):
-        if self.is_queue_empty():
-            return 'error'
+    def push_usual_goblin(self, number):
+        if self.equilibrium_condition():
+            if len(self.right_part_deque) == 0:
+                self.left_part_deque.append(number)
+            else:
+                self.left_part_deque.append(self.right_part_deque.popleft())
+                self.right_part_deque.append(number)
         else:
-            first_elem = self.list_numbers[self.start]
-            self.start += 1
-            return first_elem
+            self.right_part_deque.append(number)
 
-    def front(self):
-        if self.is_queue_empty():
-            return 'error'
+    def push_authoritative_goblin(self, number):
+        if self.equilibrium_condition():
+            self.left_part_deque.append(number)
+
         else:
-            return self.list_numbers[self.start]
+            self.right_part_deque.appendleft(number)
 
-    def size(self):
-        return self.finish - self.start
+    def remove_from_queue(self):
+        self.cured_numbers_goblins.append(self.left_part_deque.popleft())
 
-    def is_queue_empty(self):
-        return self.start == self.finish
-
-    def clear(self):
-        self.start = self.finish = 0
-        return 'ok'
-
-    def check_stack_overflow(self):
-        return self.max_size_queue == self.finish
+        if not self.equilibrium_condition():
+            self.left_part_deque.append(self.right_part_deque.popleft())
 
     def main_program(self, instruction):
         if instruction[0] == '+':
-            self.push(int(instruction[1]))
+            self.push_usual_goblin(int(instruction[1]))
 
         elif instruction[0] == '*':
-            median_index = ceil((self.start + self.finish) / 2)
-            second_half_queue = self.list_numbers[median_index:self.finish]
-            self.list_numbers[median_index] = int(instruction[1])
-            self.finish += 1
-            self.list_numbers[median_index + 1:self.finish] = second_half_queue
+            self.push_authoritative_goblin(int(instruction[1]))
 
         elif instruction[0] == '-':
-            self.cured_numbers_goblins.append(self.pop())
+            self.remove_from_queue()
 
 
 if __name__ == '__main__':
     number_commands = int(input())
-    queue = UnlimitedQueue()
+    queue = GoblinsDeque()
     for i in range(number_commands):
         command = input().split()
         queue.main_program(command)
 
     for i in range(len(queue.cured_numbers_goblins)):
         print(queue.cured_numbers_goblins[i])
+
+
 
 
